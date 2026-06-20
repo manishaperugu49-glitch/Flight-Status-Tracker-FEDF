132bc9487f5c31601e5ee437fcd6c9fa9283b8c2
Manisha Perugu <manishaperugu49@gmail.com>
2026-06-20 08:39:38 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
+import React from "react";
+
 function About() {
   return (
+    <div className="app-page">
+  
+        <div className="glass-container glass-large">
 
-      <div className="about-page">
+          <h1>✈ About Us</h1>
 
-      <h1>About Us</h1>
-       
-      <p>
-        Flight Status Tracker is designed to help users
-        easily check and monitor flight information in
-        one place. The website provides quick access to
-        flight schedules, arrival timings, departure
-        details, delays, and gate information.
-      </p>
+          <p>
+            Flight Status Tracker is designed to help users
+            easily check and monitor flight information in one place.
+            The website provides quick access to flight schedules,
+            arrival timings, departure details, delays, and gate
+            information.
+          </p>
 
-      <p>
-        Our platform aims to make flight tracking simple,
-        fast, and convenient for passengers, visitors,
-        and airport users. Users can search for flights
-        and stay updated with the latest flight status
-        information anytime.
-      </p>
+          <p>
+            Our platform aims to make flight tracking simple,
+            fast, and convenient for passengers, visitors,
+            and airport users. Users can search for flights
+            and stay updated with the latest flight status
+            information anytime.
+          </p>
 
-      <p>
-        The website focuses on providing a smooth and
-        user-friendly experience with easy navigation
-        and organized flight details for better usability.
-      </p>
+          <p>
+            The website focuses on providing a smooth and
+            user-friendly experience with easy navigation
+            and organized flight details for better usability.
+          </p>
 
-    </div>
-  )
+        </div>
+
+      </div>
+    
+  );
 }
 
-export default About
+export default About;
