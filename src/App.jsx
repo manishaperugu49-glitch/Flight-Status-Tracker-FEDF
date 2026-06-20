735cf86fe5c2716f32e4566282d0ff0f5f417813
Manisha Perugu <manishaperugu49@gmail.com>
2026-06-20 09:09:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Footer from './pages/Footer'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Notification from './pages/Notification';
+import SearchFlights from "./pages/SearchFlights";
+import AdminPanel from "./pages/AdminPanel";
+import Dashboard from "./pages/Dashboard";
+
 import "./App.css";
 function App() {
   return (
@@ -14,12 +19,16 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Home />} />
+        <Route path="/notification" element={<Notification />} />
         <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/footer" element={<Footer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/search" element={<SearchFlights />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
 
       <Footer />
